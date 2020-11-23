dda46304d52a58183de9f26481e910d6ee40377a
V4Dd3R <38853090+V4Dd3R@users.noreply.github.com>
2020-11-23 08:52:30 +0100

    Add files via upload
    Initial commit: Python code + Plot (23. Nov. 2020)

---
+++ Covid_plot.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from scipy.signal import savgol_filter
+#######################################
+# Einlesen der Daten
+data = pd.read_csv('https://opendata.ecdc.europa.eu/covid19/casedistribution/csv',
+                   sep=','
+                   )
+# Für anderes Land andere geoId verwenden
+subset_DE = data[data.geoId == 'DE']
+# Plot (benötigt Plotly-Installation)
+fig = make_subplots()
+# Glätten der Zahlen über Savitzky-Golay-Filter (Zeitraum von 3 Wochen, 2. Ordnung)
+fig.add_trace(go.Scatter(
+    x=subset_DE.dateRep,                                                            # Daten für x-Achse
+    y=savgol_filter(subset_DE.cases, 21, 2),                                        # Daten für y-Achse
+    name='Fallzahlen geglättet'                                                     # Name der Datenreihe
+))
+fig.add_trace(go.Scatter(
+    x=subset_DE.dateRep,                                                            # Daten für x-Achse
+    y=subset_DE.cases,                                                              # Daten für y-Achse
+    name='Übermittelte Fälle'                                                       # Name der Datenreihe
+))
+fig.update_traces(hoverinfo='x+y', mode='lines')                                    # Hover-Verhalten und Line-Modus
+fig.update_layout(
+            legend=dict(y=0.5, font_size=16),                                       # Legende auf halber Höhe (y=0,5)
+            title='Tägliche Covid-19 Infektionen Deutschland',                      # Titel des Plots
+            title_x=0.5,                                                            # Titel zentrieren
+            xaxis_title='Datum',                                                    # Achsenbeschriftung
+            yaxis_title='Anzahl Neuinfektionen pro Tag',
+            font=dict(                                                              # Schriftgröße und -Art
+                family='Arial',
+                size=18
+            ))
+fig.update_xaxes(autorange="reversed", tickangle=45, dtick=28)
+fig.write_html('CovidDE.html',                                                      # Erzeugt html file diesen Namens
+               auto_open=True,                                                      # File sofort öffnen (Browser)
+               include_plotlyjs='directory')                                        # Plotly-js file nur 1x erzeugen
