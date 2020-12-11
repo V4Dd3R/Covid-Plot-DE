1897496904d0969410f2bf921eb245562aaf6587
Jonathan Sturm <JonathanSturm@gmx.de>
2020-12-11 09:32:52 +0100

    Tägliche Todesfälle hinzugefügt. Standard-Exportformat geändert und Zeichen-Tool in Config hinzugefügt.

--- Covid_plot.py
+++ Covid_plot.py
@@ -4,6 +4,7 @@ import numpy as np
 from plotly.subplots import make_subplots
 from scipy.signal import savgol_filter
 from datetime import date
+
 #######################################
 # Einlesen der Daten
 today = date.today()
@@ -13,39 +14,69 @@ data = pd.read_csv('https://opendata.ecdc.europa.eu/covid19/casedistribution/csv
 # Für anderes Land andere geoId verwenden
 subset_DE = data[data.geoId == 'DE']
 # Plot (benötigt Plotly-Installation)
-fig = make_subplots()
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+config = {
+    'displaylogo': False,
+    'toImageButtonOptions': {
+        'format': 'svg',  # png, svg, jpeg, webp
+        'filename': 'Covid_DE ' + today.strftime("%d %b %Y"),
+        'height': 771,
+        'width': 1200,
+        'scale': 1
+    },
+    'modeBarButtonsToAdd': ['drawline',
+                            'drawopenpath',
+                            'drawclosedpath',
+                            'drawcircle',
+                            'drawrect',
+                            'eraseshape'
+                            ]
+}
 # Glätten der Zahlen über Savitzky-Golay-Filter (Zeitraum von 3 Wochen, 2. Ordnung)
 fig.add_trace(go.Scatter(
-    x=subset_DE.dateRep,                                                            # Daten für x-Achse
-    y=np.round(savgol_filter(subset_DE.cases, 21, 2)),                              # Daten für y-Achse
-    name='Fallzahlen geglättet',                                                    # Name der Datenreihe
-    mode='lines'
-))
+    x=subset_DE.dateRep,  # Daten für x-Achse
+    y=round(subset_DE.cases),  # Daten für y-Achse
+    name='Tägliche Infektionen',  # Name der Datenreihe
+    mode='lines+markers'),
+    secondary_y=False)
 fig.add_trace(go.Scatter(
-    x=subset_DE.dateRep,                                                            # Daten für x-Achse
-    y=round(subset_DE.cases),                                                       # Daten für y-Achse
-    name='Übermittelte Fälle',                                                      # Name der Datenreihe
-    mode='lines+markers'
-))
-fig.update_traces(hoverinfo='x+y')                                                  # Hover-Verhalten
+    x=subset_DE.dateRep,  # Daten für x-Achse
+    y=np.round(savgol_filter(subset_DE.cases, 21, 2)),  # Daten für y-Achse
+    name='Tägliche Infektionen (geglättet)',  # Name der Datenreihe
+    mode='lines'),
+    secondary_y=False)
+fig.add_trace(go.Scatter(
+    x=subset_DE.dateRep,  # Daten für x-Achse
+    y=round(subset_DE.deaths),  # Daten für y-Achse
+    name='Tägliche Todesfälle',  # Name der Datenreihe
+    mode='lines+markers'),
+    secondary_y=True)
+fig.add_trace(go.Scatter(
+    x=subset_DE.dateRep,  # Daten für x-Achse
+    y=np.round(savgol_filter(subset_DE.deaths, 21, 2)),  # Daten für y-Achse
+    name='Tägliche Todesfälle (geglättet)',  # Name der Datenreihe
+    mode='lines'),
+    secondary_y=True)
+fig.update_traces(hoverinfo='x+y')  # Hover-Verhalten
 fig.update_yaxes(exponentformat='none', tickformat='{}')
 fig.update_layout(
-            legend=dict(font_size=16),                                              # Legende
-            title_font=dict(size=18),
-            title=('<b>Tägliche Covid-19 Infektionen Deutschland</b> <br>' +        # Titel des Plots
-                   'Stand: ' + today.strftime("%d %b %Y") +
-                   ' <a style="font-size:16px"; href="https://opendata.ecdc.europa.eu/">(Datenquelle)</a>' +
-                   ' <a style="font-size:16px"; href="https://github.com/V4Dd3R/Covid-Plot-DE">(Quellcode)</a>'),
-            title_x=0.5,                                                            # Titel zentrieren
-            xaxis_title='Datum',                                                    # Achsenbeschriftung
-            yaxis_title='Anzahl Neuinfektionen pro Tag',
-            font=dict(                                                              # Schriftgröße und -Art
-                family='Arial',
-                size=16
-            ),
-            plot_bgcolor="LightGray"
+    legend=dict(font_size=16),  # Legende
+    title_font=dict(size=18),
+    title=('<b>Tägliche Covid-19 Infektionen Deutschland</b> <br>' +  # Titel des Plots
+           'Stand: ' + today.strftime("%d %b %Y") +
+           ' <a style="font-size:16px"; href="https://opendata.ecdc.europa.eu/">(Datenquelle)</a>' +
+           ' <a style="font-size:16px"; href="https://github.com/V4Dd3R/Covid-Plot-DE">(Quellcode)</a>'),
+    title_x=0.5,  # Titel zentrieren
+    xaxis_title='Datum',  # Achsenbeschriftung
+    yaxis_title='Anzahl Neuinfektionen pro Tag',
+    font=dict(  # Schriftgröße und -Art
+        family='Arial',
+        size=16
+    ),
+    plot_bgcolor="LightGray"
 )
 fig.update_xaxes(autorange="reversed", tickangle=45, dtick=28)
-fig.write_html('index.html',                                                        # Erzeugt html file diesen Namens
-               auto_open=True,                                                      # File sofort öffnen (Browser)
-               include_plotlyjs='True')                                             # Plotly-js in .html integrieren
+fig.write_html('index.html',  # Erzeugt html file diesen Namens
+               auto_open=True,  # File sofort öffnen (Browser)
+               include_plotlyjs='True',  # Plotly-js in .html integrieren
+               config=config)
